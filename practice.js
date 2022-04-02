b336b36b5a06c804eef08ca2febe353451df2374
lexus654 <ronald.laz@tup.edu.ph>
2022-04-02 16:46:50 +0800

    https://github.com/lexus654/CodeWars-.git

---
+++ practice.js
@@ -0,0 +1,35 @@
+// const words = "How can mirrors be real if our eyes aren't real";
+// // steps 1. divide the words 2. create new word
+// const arr = words.split(" ");
+// const Newarr = [];
+
+// for (i = 0; i < arr.length; i++) {
+//   Newarr.push(arr[i].charAt(0).toLocaleUpperCase() + arr[i].slice(1));
+// }
+
+// console.log(Newarr.join(" "));
+
+// const firstWord = function (sentence) {
+//   const newArr = [];
+//   const newSentence = [];
+//   newArr.push(...sentence.split(" "));
+//   for (i = 0; i < newArr.length; i++) {
+//     newSentence.push(
+//       newArr[i].charAt(0).toLocaleUpperCase() + newArr[i].slice(1)
+//     );
+//   }
+//   return newSentence.join(" ");
+// };
+
+const firstWord = function (sentence) {
+  const newSentence = [];
+  for (i = 0; i < sentence.split(" ").length; i++) {
+    newSentence.push(
+      sentence.split(" ")[i].charAt(0).toLocaleUpperCase() +
+        sentence.split(" ")[i].slice(1)
+    );
+  }
+  return newSentence.join(" ");
+};
+
+console.log(firstWord("hello me ronald"));
